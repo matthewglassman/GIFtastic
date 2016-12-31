c9c6574fc6b890780e41124a00e7833dc3ad2587
matthewglassman <matthewglassmn78@gmail.com>
2016-12-31 13:28:00 -0500

    Created code for dynamic buttons

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,2 +1,21 @@
 //Intiail Array of Standups
-var comic = ["Richard Pryor", "John Pinette", "Amy Schumer", "Dave Chappelle", "Kathleen Madigan"];
+var comics = ["Richard Pryor", "John Pinette", "Amy Schumer", "Dave Chappelle", "Kathleen Madigan"];
+
+//function to create buttons dynamically
+function buttonCreation() {
+	//Deleting the button prior to adding a new one.
+	//This is needed so we won't get repeat buttons.
+
+	$("#comedianButton").empty();
+
+	//Looping through the array of comics
+	for (var i = 0; i < comic.length; i++) {
+		// Dynamically creating the buttons
+		var comicName = $("<button>");
+		//adding a class
+		comicName.addClass("comedian");
+		comicName.attr("data-name", comics[i]);
+		comicName.text(comics[i]);
+		$("#comedianButton").append(comicName);
+	}
+}
