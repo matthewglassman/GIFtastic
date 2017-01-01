758549f102e26891cd6b6d0a47bb7b5a35f452ea
matthewglassman <matthewglassmn78@gmail.com>
2017-01-01 14:28:53 -0500

    Created functionality to add a button from text field. Next utilizing Giphy API

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,20 +1,20 @@
 //Intial Array of Standups
 var comics = ["Richard Pryor", "John Pinette", "Amy Schumer", "Dave Chappelle", "Kathleen Madigan"];
 
-function renderButtons(){
-	$("#buttons-view").empty();
+function renderButtons() {
+	$("#comedianButton").empty();
 	for(var i = 0; i < comics.length; i++) {
 		var a = $("<button>");
 		a.addClass("comic");
 		a.attr("data-name", comics[i]);
 		a.text(comics[i]);
-		$("#buttons-view").append(a);
+		$("#comedianButton").append(a);
 	}
 }
 
-$("#add-comic").on("click", function(event){
+$("#addComic").on("click", function(event) {
 	event.preventDefault();
-	var comedian = $("#comic-input").val().trim();
+	var comedian = $("#standupComic").val().trim();
 	comics.push(comedian);
 
 renderButtons();
