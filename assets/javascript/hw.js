9b5913468ef0f976d52be5e9293fa46aae75e9e4
matthewglassman <matthewglassmn78@gmail.com>
2017-01-01 20:49:47 -0500

    Added Giphy API and testing GIF creation with buttons rendered at load time

--- assets/javascript/hw.js
+++ assets/javascript/hw.js
@@ -1,3 +1,5 @@
+//GIPHY API public key dc6zaTOxFJmzC
+
 //Intial Array of Standups
 var comics = ["Richard Pryor", "John Pinette", "Amy Schumer", "Dave Chappelle", "Kathleen Madigan"];
 
@@ -12,12 +14,35 @@ function renderButtons() {
 	}
 }
 
-$("#addComic").on("click", function(event) {
-	event.preventDefault();
-	var comedian = $("#standupComic").val().trim();
-	comics.push(comedian);
-
-renderButtons();
+$("button").on("click", function() {
+	$("#comicGIF").empty();
+	var comedian = $(this).data("name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + comedian + "&api_key=dc6zaTOxFJmzC&limit=10";
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.done(function(response){
+		var results = response.data;
+		for (var i = 0; i < results.length; i++){
+			var standupDiv = $("<div class='standup'>");
+			var rating = results[i].rating;
+			var p = $("<p>").text("Rating: " + rating);
+			var comicsImage = $("<img>");
+			comicsImage.attr("src", results[i].images.fixed_height.url);
+			standupDiv.prepend(p);
+			standupDiv.prepend(comicsImage);
+			$("#comicGIF").prepend(standupDiv);
+		}
+	});
 });
+
+// $("#addComic").on("click", function(event) {
+// 	event.preventDefault();
+// 	var comedian = $("#standupComic").val().trim();
+// 	comics.push(comedian);
+
+// renderButtons();
+// });
 
 renderButtons();
